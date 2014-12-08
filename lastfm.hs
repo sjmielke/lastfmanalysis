91b2ca131abff54794e147c9d75f7dc5a620f8e2
Sebastian J. Mielke <sjm@sjmielke.com>
2014-12-08 23:43:49 +0100

    Refactoring for/and more efficient downloading. Time to get to work... tomorrow.

--- lastfm.hs
+++ lastfm.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Aeson
-import Data.Aeson.Types (Value, Parser, parse)
-
-import qualified Data.ByteString.Lazy.Char8 as BSC
-import Data.Aeson.Encode.Pretty
+import Data.Aeson.Types (parse)
 
 import Control.Applicative
-import Data.Text (pack)
+import Data.Text (Text, pack)
 import Data.HashMap.Strict (fromList)
+import GHC.Int (Int64)
 
 import qualified Network.Lastfm as LFM
 import qualified Network.Lastfm.User as LFM.User
@@ -27,25 +25,34 @@ instance FromJSON Scrobble where
             <*> (v .: "album" >>= (.: "#text"))
         where stddate = (fromList [("uts", "42")]) -- now playing doesn't have a "date"
 
+getScrobblePage :: LFM.Connection -> Text -> Text -> Int64 -> IO [Scrobble]
+getScrobblePage con userName apiKey page = do
+        putStrLn $ "Getting page " ++ show page
+        response <- LFM.lastfm con $ LFM.User.getRecentTracks
+                                  <*> LFM.user userName
+                                  <*> LFM.apiKey apiKey
+                                  <* LFM.json
+                                  <* LFM.page page
+                                  <* LFM.limit 200
+        let res = case response of
+                      (Right res) -> res
+                      (Left e) -> error $ show e
+
+        let trackListExtractor o = parseJSON o
+                               >>= (.: "recenttracks")
+                               >>= (.: "track")
+        let scrobbleList = case parse trackListExtractor res of
+                               (Success sl) -> sl :: [Scrobble]
+                               (Error e) -> error e
+        
+        return scrobbleList
+
 main = do putStr "User name: "
           userName <- fmap pack getLine
           
           con <- LFM.newConnection
-          response <- LFM.lastfm con $ LFM.User.getRecentTracks
-                                    <*> LFM.user userName
-                                    <*> LFM.apiKey "e38cc7822bd7476fe4083e36ee69748e" -- isn't even mine, heh.
-                                    <* LFM.json
-                                    <* LFM.from 0
-          let res = case response of
-                        (Right res) -> res
-                        (Left e) -> error $ show e
+          scrobbleList <- fmap concat $ mapM (getScrobblePage con userName "e38cc7822bd7476fe4083e36ee69748e") [1..111]
           
-          -- BSC.putStrLn . encodePretty $ res
+          writeFile "/tmp/scrobblelist" $ show scrobbleList
           
-          let trackListExtractor o = parseJSON o
-                                 >>= (.: "recenttracks")
-                                 >>= (.: "track")
-          let scrobbleList = case parse trackListExtractor res of
-                                 (Success sl) -> sl :: [Scrobble]
-                                 (Error e) -> error e
-          print $ scrobbleList
+          print $ length $ scrobbleList
