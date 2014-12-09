ba7e217f28882015236c1c287fa7cbc41ec1acc4
Sebastian J. Mielke <sjm@sjmielke.com>
2014-12-09 15:17:11 +0100

    Analysis: since last christmas, which tracks have I listened to for the first time?

--- lastfm.hs
+++ lastfm.hs
@@ -4,6 +4,9 @@ import Data.Aeson
 import Data.Aeson.Types (parse)
 
 import Control.Applicative
+import Data.Function (on)
+import Data.Ord (comparing)
+import Data.List (deleteFirstsBy, nubBy, groupBy, sortBy)
 import Data.Text (Text, pack)
 import Data.HashMap.Strict (fromList)
 import GHC.Int (Int64)
@@ -15,7 +18,7 @@ data Scrobble = Scrobble { timestamp :: Int
                          , title :: String
                          , artist :: String
                          , album :: String
-                         } deriving (Eq, Show)
+                         } deriving (Eq, Show, Read)
 
 instance FromJSON Scrobble where
     parseJSON (Object v) = Scrobble
@@ -23,7 +26,10 @@ instance FromJSON Scrobble where
             <*> (v .: "name")
             <*> (v .: "artist" >>= (.: "#text"))
             <*> (v .: "album" >>= (.: "#text"))
-        where stddate = (fromList [("uts", "42")]) -- now playing doesn't have a "date"
+        where stddate = (fromList [("uts", "42")]) -- now playing doesn't have a "date" - we filter this later
+
+sameTrack :: Scrobble -> Scrobble -> Bool
+sameTrack (Scrobble _ t1 a1 _) (Scrobble _ t2 a2 _) = t1 == t2 && a1 == a2
 
 getScrobblePage :: LFM.Connection -> Text -> Text -> Int64 -> IO [Scrobble]
 getScrobblePage con userName apiKey page = do
@@ -45,14 +51,45 @@ getScrobblePage con userName apiKey page = do
                                (Success sl) -> sl :: [Scrobble]
                                (Error e) -> error e
         
-        return scrobbleList
+        -- Remove the "mow playing" entry marked with 42 above
+        return $ filter ((/=42) . timestamp) scrobbleList
 
-main = do putStr "User name: "
+main = do {- Retrieving data takes far too long, so...
+          putStr "User name: "
           userName <- fmap pack getLine
           
           con <- LFM.newConnection
           scrobbleList <- fmap concat $ mapM (getScrobblePage con userName "e38cc7822bd7476fe4083e36ee69748e") [1..111]
           
-          writeFile "/tmp/scrobblelist" $ show scrobbleList
+          writeFile "/home/sjm/scrobblelist" $ show scrobbleList
           
-          print $ length $ scrobbleList
+          -- load the scrobblelist from a file \o/ -}
+          scrobbleListFile <- readFile "/home/sjm/downloads/scrobblelist"
+          let scrobbleList = read scrobbleListFile
+          
+          let lastChristmas = 1387497600
+          
+          let (newScrobbles, oldScrobbles) = span ((>lastChristmas) . timestamp) scrobbleList
+          
+          let realNewScrobbles = deleteFirstsBy sameTrack
+                                                (nubBy sameTrack newScrobbles)
+                                                (nubBy sameTrack oldScrobbles)
+          
+          putStrLn $ "Unique track count: " ++ show (length $ nubBy sameTrack scrobbleList)
+          putStrLn $ "Unique new tracks: " ++ show (length realNewScrobbles)
+          
+          let partitionWithAttribute accessor = groupBy ((==) `on` accessor)
+                                              . sortBy (comparing accessor)
+          
+          let artists :: [[Scrobble]]
+              artists = partitionWithAttribute artist realNewScrobbles
+          let artistsAndTheirAlbums :: [[[Scrobble]]]
+              artistsAndTheirAlbums = map (partitionWithAttribute album) artists
+          let albums :: [[Scrobble]]
+              albums = partitionWithAttribute album realNewScrobbles
+          
+          writeFile "/home/sjm/downloads/realnewtracks" $ printBeautifully $ map (\a -> (album $ head a, map title a)) albums
+
+printBeautifully :: [(String, [String])] -> String
+printBeautifully = concatMap printSingle
+    where printSingle (x, ss) = ("\n" ++ x ++ "\n") ++ concatMap (('\t':) . (++"\n")) ss
