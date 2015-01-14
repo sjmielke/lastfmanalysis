5d948bbfab78e078e40cfd98eb71ea2f5f5e0685
Sebastian J. Mielke <sjm@sjmielke.com>
2015-01-14 22:10:34 +0100

    Printing out an "image" myself on the console. And it looks way less shitty than I anticipated :O

--- ArtistScores.hs
+++ ArtistScores.hs
@@ -3,11 +3,44 @@ module ArtistScores where
 import LastFM
 import Lengths
 
+import Control.Arrow (second)
 import Data.Maybe (catMaybes, fromJust)
 import Data.List (sort, nub, sortBy)
 import Data.Ord (comparing)
 import Data.Time.Clock.POSIX (getPOSIXTime, posixSecondsToUTCTime)
 import qualified Database.SQLite.Simple as SQL
+import Text.Printf
+
+intToTimeString :: Int -> String
+intToTimeString = show
+                . posixSecondsToUTCTime
+                . fromIntegral
+
+normalizeArtistProgressions :: [(String, [((Int, Int), Int)])] -> [(String, [((Int, Int), Double)])]
+normalizeArtistProgressions artistProgressions = map (second $ map (second $ (/ maxVal) . fromIntegral)) artistProgressions
+    where maxVal = fromIntegral
+                 $ maximum
+                 $ map snd
+                 $ concatMap snd artistProgressions
+
+getTSV :: [(String, [((Int, Int), Int)])] -> String
+getTSV artistProgressions = outTSVHead ++ "\n" ++ outTSVBody
+    where generateTSV (a, intervals) =  a
+                                    ++ "\t"
+                                    ++ concatMap ((++"\t") . show . snd) intervals
+          outTSVHead = ("\t"++)
+                     $ concatMap ((++"\t") . intToTimeString . snd . fst)
+                     $ snd
+                     $ head artistProgressions -- assuming it found anything.
+          outTSVBody = unlines
+                     $ map generateTSV $ normalizeArtistProgressions artistProgressions
+
+printOut :: [(String, [((Int, Int), Double)])] -> String
+printOut as = unlines $ map printArtist as
+    where printArtist (a, intervals) =  printf ("%" ++ maxArtistLength ++ "s ") a
+                                     ++ map getCharFor intervals
+          maxArtistLength = show $ maximum $ map (length . fst) as
+          getCharFor (_, x) = "░▒▓█" !! (min 3 $ round $ x * 4)
 
 main = do conn <- SQL.open "/home/sjm/.config/Clementine/clementine.db"
           
@@ -32,9 +65,6 @@ main = do conn <- SQL.open "/home/sjm/.config/Clementine/clementine.db"
           
           now <- fmap round getPOSIXTime
           
-          let intToTimeString = show
-                              . posixSecondsToUTCTime
-                              . fromIntegral
           let getText f ((start, end), val) = putStrLn $  (intToTimeString start)
                                                        ++ " - "
                                                        ++ (intToTimeString end)
@@ -52,37 +82,22 @@ main = do conn <- SQL.open "/home/sjm/.config/Clementine/clementine.db"
                                              (2 * 3600)
                                              0.4
                                              scrobblesWithLength
-          mapM_ (getText show) hyped
+          -- mapM_ (getText show) hyped
           
           let allEverHypedArtists = sort $ nub $ map fst $ catMaybes $ map snd hyped
           
           let artistProgressions = zip allEverHypedArtists
                                  $ map ( \a -> getLengthsPerFrom
                                                (+ 14 * 24 * 3600)
-                                               (const $ timestamp . last $ scrobbleList)
+                                               (const $ timestamp . fst . last $ scrobblesWithLength)
                                                now
                                              $ filter (\(x, _) -> artist x == a) 
                                                       scrobblesWithLength
                                        )
                                        allEverHypedArtists
           
-          {-
-          mapM_ (getText $ show . (`div` 3600)) $ fromJust
-                                                $ lookup "World's End Girlfriend"
-                                                $ artistProgressions
-          -- -}
+          writeFile "/tmp/hyped.tsv" $ getTSV artistProgressions
           
-          let generateTSV (a, intervals) =  a
-                                        ++ "\t"
-                                        ++ concatMap ((++"\t") . show . snd) intervals
-          
-          let outTSVHead = ("\t"++)
-                         $ concatMap ((++"\t") . intToTimeString . snd . fst)
-                         $ snd
-                         $ head artistProgressions -- assuming it found anything.
-          let outTSVBody = unlines
-                         $ map generateTSV artistProgressions
-          
-          writeFile "/tmp/hyped.tsv" $ outTSVHead ++ "\n" ++ outTSVBody
+          putStrLn $ printOut $ normalizeArtistProgressions artistProgressions
           
           SQL.close conn
