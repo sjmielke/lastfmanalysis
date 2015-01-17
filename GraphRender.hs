b0944f11352717df0a8c56f9aa393fd3112a4f28
Sebastian J. Mielke <sjm@sjmielke.com>
2015-01-17 12:43:52 +0100

    Only add borders to entries from the first time the artist appears.

--- GraphRender.hs
+++ GraphRender.hs
@@ -41,14 +41,21 @@ main = do artistProgressions <- getArtistProgressions
                    )
                    |||
                    strutX 0.2
-          line (a, is) = (artistname 1 a ||| hcat (map (cell a) is) ||| artistname 0 a)
+          line (a, is) = (artistname 1 a |||
+                            (borders is # center <> hcat (map (cell a) is) # center)
+                         ||| artistname 0 a)
                        <> strutY 0.2
           artistname align name = (strutX (abs $ align - 0.1) |||
                                     (alignedText align 0.5 name # fontSize (Local 0.05)
                                                                 # font "Helvetica Neue"
                                      <> strutY 0.1)
                                   ||| strutX (abs $ align - 0.9))
-          cell a (_,x) = rect 0.1 0.15 # fc (uncurryRGB rgb $ hsl h s l) # lwL 0.001
-            where h = fromIntegral $ sum (map ord a) `mod` 360
-                  s = 0.8
-                  l = 1 - sqrt x
+          cell a (_,x) = rect 0.1 0.15 # fc (uncurryRGB rgb $ hsl h s l)
+                                       # lwL 0
+              where h = fromIntegral $ sum (map ord a) `mod` 360
+                    s = 0.8
+                    l = 1 - sqrt x
+          borders is = strutX (emptystarters * 0.1) ||| grid (length is - round emptystarters)
+              where emptystarters = genericLength $ takeWhile ((==0) . snd) is
+          grid n =  rect (fromIntegral n * 0.1) 0.15 # lwL 0.003 # lc gray
+                 <> center (hcat $ replicate n $ strutX 0.1 ||| vrule 0.15 # lwL 0.003 # lc gray)
